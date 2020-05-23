ecceb71b54980a896168b1d49adabf01afd84d4e
PiNdoye94 <papisndoye218@gmail.com>
2020-05-24 00:57:33 +0100

    implémentation exercice 1 en python

---
+++ ExoAlgo1.py
@@ -0,0 +1,5 @@
+a = int(input("entrer la valeur de a: "))
+b = int(input("entrer la valeur de b: "))
+print("le quotient entier de la division est: ", a//b)
+print("le quotient reel de la division est: ", a/b)
+print("le reste de la division est : ", a%b)
